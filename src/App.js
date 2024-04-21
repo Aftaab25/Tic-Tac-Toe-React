16156889c46c9e7d7d1f501a322e7d04096e8a81
aftaab25 <aftaab2507@gmail.com>
2024-04-22 00:31:56 +0530

    Added logic to Declare Winner

--- src/App.js
+++ src/App.js
@@ -9,16 +9,32 @@ function Square({ value, handleClick }) {
 }
 
 export default function Board() {
+  const [isXNext, setIsXNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function onSquareClick(i) {
+    if (squares[i] || checkWinner(squares)) return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    nextSquares[i] = isXNext ? "X" : "O";
+
+    setIsXNext(!isXNext);
     setSquares(nextSquares);
+  }
+
+  const winner = checkWinner(squares);
+  let status;
+
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (isXNext ? "X" : "O");
   }
 
   return (
     <>
+      <div className="status">{status}</div>
+
       <div className="board-row">
         <Square value={squares[0]} handleClick={() => onSquareClick(0)} />
         <Square value={squares[1]} handleClick={() => onSquareClick(1)} />
@@ -38,4 +54,27 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function checkWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
